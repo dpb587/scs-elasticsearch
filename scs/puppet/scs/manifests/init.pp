b116179fadccf8b516b4c0b2f0e7f7224159ac04
Danny Berger <dpb587@gmail.com>
2014-02-06 19:31:16 -0700

    restructuring

--- scs/puppet/scs/manifests/init.pp
+++ scs/puppet/scs/manifests/init.pp
@@ -1,8 +1,27 @@
 class scs (
-    $version = '0.90.10',
+    $download_tgz = undef,
+    $download_zip = undef,
+    $download_tar = undef,
+    $version = '1.0.0',
     $cluster = 'default',
+    $es_env_opts = {},
+    $es_run_args = [],
     $options = {},
 ) {
+    if undef != $download_tar {
+        $download_url = $download_tar
+        $download_command = '/usr/bin/wget -qO- "$DOWNLOAD" | /bin/tar -x --strip-components 1'
+    } elsif undef != $download_tgz {
+        $download_url = $download_tgz
+        $download_command = '/usr/bin/wget -qO- "$DOWNLOAD" | /bin/tar -xz --strip-components 1'
+    } elsif undef != $download_zip {
+        $download_url = $download_zip
+        $download_command = '/usr/bin/wget "$DOWNLOAD" && /usr/bin/unzip -d .scstmp `basename "$DOWNLOAD"` && /bin/mv .scstmp/*/* ./ && /bin/rm -fr `basename "$DOWNLOAD"` .scstmp'
+    } elsif undef != $version {
+        $download_url = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${version}.tar.gz"
+        $download_command = '/usr/bin/wget -qO- "$DOWNLOAD" | /bin/tar -xz --strip-components 1'
+    }
+
     group {
         'scs' :
             ensure => present,
@@ -22,13 +41,26 @@ class scs (
             ;
     }
 
+    file {
+        '/scs/usr/elasticsearch' :
+            ensure => directory,
+            owner => 'scs',
+            group => 'scs',
+            mode => 0700,
+            ;
+    }
+
     exec {
         'elasticsearch' :
-            command => "/usr/bin/wget -O- https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${version}.tar.gz | /bin/tar -xz && /bin/mv elasticsearch-* elasticsearch",
-            cwd => '/scs/usr',
+            command => $download_command,
+            environment => [
+                "DOWNLOAD=${download_url}",
+            ],
+            cwd => "/scs/usr/elasticsearch",
+            creates => "/scs/usr/elasticsearch/bin/elasticsearch",
             require => [
-                Package['wget'],
-                File['/scs/usr'],
+                File["/scs/usr/elasticsearch"],
+                Package['unzip'],
             ],
             ;
         'apt-update' :
@@ -109,6 +141,12 @@ class scs (
             ],
             ;
         'wget' :
+            ensure => installed,
+            require => [
+                Exec['apt-update'],
+            ],
+            ;
+        'unzip' :
             ensure => installed,
             require => [
                 Exec['apt-update'],
